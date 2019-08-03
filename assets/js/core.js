49607cc42e4ab831dfcfbb3335eba78d9e8df2e3
Ahmet Emin ŞİT <sitahmetemin@hotmail.com>
2019-08-04 01:44:50 +0300

    anket düzenleme sayfası değişiklikleri

--- assets/js/core.js
+++ assets/js/core.js
@@ -67,42 +67,45 @@ $(document).ready(function () {
             $("*.menu-caret-icon").removeClass("fa-caret-right");
         }
     }
-    $('#bootstrapDataTable').dataTable({
-        responsive: true,
-        searching: false,
-        "lengthChange": false,
-        "language": {
-            "sDecimal":        ",",
-            "sEmptyTable":     "Tabloda herhangi bir veri mevcut değil",
-            "sInfo":           "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
-            "sInfoEmpty":      "Kayıt yok",
-            "sInfoFiltered":   "(_MAX_ kayıt içerisinden bulunan)",
-            "sInfoPostFix":    "",
-            "sInfoThousands":  ".",
-            "sLengthMenu":     "Sayfada _MENU_ kayıt göster",
-            "sLoadingRecords": "Yükleniyor...",
-            "sProcessing":     "İşleniyor...",
-            "sSearch":         "Ara:",
-            "sZeroRecords":    "Eşleşen kayıt bulunamadı",
-            "oPaginate": {
-                "sFirst":    "İlk",
-                "sLast":     "Son",
-                "sNext":     "Sonraki",
-                "sPrevious": "Önceki"
-            },
-            "oAria": {
-                "sSortAscending":  ": artan sütun sıralamasını aktifleştir",
-                "sSortDescending": ": azalan sütun sıralamasını aktifleştir"
-            },
-            "select": {
-                "rows": {
-                    "_": "%d kayıt seçildi",
-                    "0": "",
-                    "1": "1 kayıt seçildi"
+    if ($('#bootstrapDataTable').length !== 0) {
+        $('#bootstrapDataTable').dataTable({
+            responsive: true,
+            searching: false,
+            "lengthChange": false,
+            "language": {
+                "sDecimal": ",",
+                "sEmptyTable": "Tabloda herhangi bir veri mevcut değil",
+                "sInfo": "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
+                "sInfoEmpty": "Kayıt yok",
+                "sInfoFiltered": "(_MAX_ kayıt içerisinden bulunan)",
+                "sInfoPostFix": "",
+                "sInfoThousands": ".",
+                "sLengthMenu": "Sayfada _MENU_ kayıt göster",
+                "sLoadingRecords": "Yükleniyor...",
+                "sProcessing": "İşleniyor...",
+                "sSearch": "Ara:",
+                "sZeroRecords": "Eşleşen kayıt bulunamadı",
+                "oPaginate": {
+                    "sFirst": "İlk",
+                    "sLast": "Son",
+                    "sNext": "Sonraki",
+                    "sPrevious": "Önceki"
+                },
+                "oAria": {
+                    "sSortAscending": ": artan sütun sıralamasını aktifleştir",
+                    "sSortDescending": ": azalan sütun sıralamasını aktifleştir"
+                },
+                "select": {
+                    "rows": {
+                        "_": "%d kayıt seçildi",
+                        "0": "",
+                        "1": "1 kayıt seçildi"
+                    }
                 }
             }
-        }
-    });
+        });
+    }
+
 });
 
 $("#sidebarToggleButton").on("click", function () {
--- assets/js/pages/anket-olusturma.js
+++ assets/js/pages/anket-olusturma.js
@@ -1,14 +1,35 @@
+// global varible
+var sectionCounter = 1;
+var pageCounter = 1;
+
 $('*#showQuestionPanel').on("click", function () {
     $('.question-type-section').toggleClass("active-question-type-section");
     $(this).toggleClass("text-success");
     $('.question-type > i').toggleClass("border-0");
     $('.question-type').toggleClass("border");
 });
-var sectionCounter = 1;
+
 $('#newSectionButton').on('click', function () {
     sectionCounter++;
-    $('#questionSectionForm').append('<section id="section' + sectionCounter + '" class="section"></section>');
-    $('.section-control-area').append('<hr class="section-hr"><div class="section-control"><a href="#section' + sectionCounter + '" class="btn btn-light btn-sm">' + sectionCounter + '</a></div>');
+    $('#questionSectionForm')
+        .append('<section id="section' + sectionCounter + '" class="section">' +
+        '<div class="section-header">' +
+            '<h3 class="mr-auto">Bölüm ' + sectionCounter + '</h3>' +
+            '<div class="dropdown">\n' +
+            '<button class="btn waves-effect" type="button" id="dropdownSectionHeader" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">\n' +
+            '<i class="fa fa-ellipsis-h"></i>\n' +
+            '</button>\n' +
+            '<div class="dropdown-menu dropdown-primary">\n' +
+            '<a id="removeSectionButton" class="dropdown-item" href="javascript:void(0)">Sil</a>\n' +
+            '</div>\n' +
+            '</div>' +
+        '</section>');
+
+    $('.section-control-area')
+        .append('<hr class="section-hr">' +
+            '<div class="section-control">' +
+            '<a href="#section' + sectionCounter + '" class="btn btn-light btn-sm">' + sectionCounter + '</a>' +
+            '</div>');
 });
 
 
@@ -30,7 +51,11 @@ $(document).on("click", ".section", function () {
 });
 
 $(document).on('click', '#appendAnswerInput', function () {
-    $('#answerSection').append('<div class="input-group mb-4"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1">Yanıt Giriniz</span></div><input type="text" class="form-control"><div class="input-group-append"><button id="removeAnswerInput" class="btn btn-md btn-outline-danger m-0 px-3 py-2 z-depth-0 waves-effect" type="button" id="button-addon2"><i class="fa fa-minus"></i></button></div></div>');
+    $(this)
+        .parent()
+        .prev()
+        .prev()
+        .append('<div class="input-group mb-4"><div class="input-group-prepend"><span class="input-group-text" id="basic-addon1">Yanıt Giriniz</span></div><input type="text" class="form-control"><div class="input-group-append"><button id="removeAnswerInput" class="btn btn-md btn-outline-danger m-0 px-3 py-2 z-depth-0 waves-effect" type="button" id="button-addon2"><i class="fa fa-minus"></i></button></div></div>');
 });
 
 $(document).on("click", '*#removeAnswerInput', function () {
@@ -48,11 +73,17 @@ $(document).ready(function () {
 
 });
 
-var pageCounter = 1;
 $('#newPageButton').on('click', function () {
     if (pageCounter < 5) {
-        $('#pageControlSection').append('<div id="buttonGroup"><button class="btn btn-success btn-sm mr-4 text-nowrap waves-effect" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sayfa ' + (pageCounter + 1) + '<i class="fa fa-ellipsis-h"></i></button><div class="dropdown-menu"><a id="removePageButton" class="dropdown-item" href="#">Sil</a></div></div>');
         pageCounter++;
+        $('#pageControlSection')
+            .append('<div id="buttonGroup">' +
+            '<button class="btn btn-success btn-sm mr-4 text-nowrap waves-effect" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Sayfa ' + pageCounter + '<i class="fa fa-ellipsis-h"></i></button>' +
+            '<div class="dropdown-menu">' +
+            '<a id="showTab" data-showcustomtab="#customTab' + pageCounter + '" class="dropdown-item" href="#">Göster</a>' +
+            '<a id="removePageButton" class="dropdown-item" href="#">Sil</a>' +
+            '</div>' +
+            '</div>');
     }
 });
 
@@ -68,61 +99,81 @@ $(document).on("click", "#removePageButton", function () {
 });
 
 $('#multipleChoiceQuestionAddButton').on("click", function () {
-    $('.section.border-primary').append('<div class="row">\n' +
-        '<div class="col-12 d-flex justify-content-center align-items-center">\n' +
-        '<label class="label label-success fs-x1-5 mr-auto" for="">Soru</label>\n' +
-        '<div class="d-flex">\n' +
-        '<button type="button" class="btn btn-white btn-outline-success waves-effect"><i class="fal fa-edit text-success"></i>Düzenle</button>\n' +
-        '<button type="button" class="btn btn-white btn-outline-success waves-effect"><i class="fal fa-bars text-success"></i>Seçenekler</button>\n' +
-        '</div>\n' +
-        '</div>\n' +
-        '</div>\n' +
-        '<hr>\n' +
-        '<div class="row">\n' +
-        '<div id="answerSection" class="col-8">\n' +
-        '<div class="input-group mb-4">\n' +
-        '<div class="input-group-prepend">\n' +
-        '<span class="input-group-text">Soru Yazınız</span>\n' +
-        '</div>\n' +
-        '<input type="text" class="form-control">\n' +
-        '</div>\n' +
-        '<div class="input-group mb-4">\n' +
-        '<div class="input-group-prepend">\n' +
-        '<span class="input-group-text">Yanıt Giriniz</span>\n' +
-        '</div>\n' +
-        '<input type="text" class="form-control">\n' +
-        '<div class="input-group-append">\n' +
-        '<button id="removeAnswerInput" class="btn btn-md btn-outline-danger m-0 px-3 py-2 z-depth-0 waves-effect" type="button"><i class="fa fa-minus"></i></button>\n' +
-        '</div>\n' +
-        '</div>\n' +
-        '<div class="input-group mb-4">\n' +
-        '<div class="input-group-prepend">\n' +
-        '<span class="input-group-text" id="basic-addon1">Yanıt Giriniz</span>\n' +
-        '</div>\n' +
-        '<input type="text" class="form-control">\n' +
-        '<div class="input-group-append">\n' +
-        '<button id="removeAnswerInput" class="btn btn-md btn-outline-danger m-0 px-3 py-2 z-depth-0 waves-effect" type="button"><i class="fa fa-minus"></i></button>\n' +
-        '</div>\n' +
-        '</div>\n' +
-        '<div class="input-group mb-4">\n' +
-        '<div class="input-group-prepend">\n' +
-        '<span class="input-group-text" id="basic-addon1">Yanıt Giriniz</span>\n' +
-        '</div>\n' +
-        '<input type="text" class="form-control">\n' +
-        '<div class="input-group-append">\n' +
-        '<button id="removeAnswerInput" class="btn btn-md btn-outline-danger m-0 px-3 py-2 z-depth-0 waves-effect" type="button"><i class="fa fa-minus"></i></button>\n' +
-        '</div>\n' +
-        '</div>\n' +
-        '</div>\n' +
-        '<div class="col-4">\n' +
-        '<select class="form-control" name="" id="">\n' +
-        '<option value="1">Seçiniz</option>\n' +
-        '<option value="1">Ahmet</option>\n' +
-        '<option value="1">Mehmet</option>\n' +
-        '</select>\n' +
-        '</div>\n' +
-        '<div class="col-12">\n' +
-        '<a id="appendAnswerInput" class=""><i class="fal fa-plus-circle mr-2"></i>Başka bir seçenek ekleniyiz</a>\n' +
-        '</div>\n' +
-        '</div>')
+
+    $('.section.border-primary')
+        .append('<section class="question-area">\n' +
+            '<div class="row">\n' +
+            '<div class="col-12 d-flex justify-content-center align-items-center">\n' +
+            '<label class="label label-success fs-x1-5 mr-auto" for="">Soru</label>\n' +
+            '<div class="d-flex">\n' +
+            '<button type="button" class="btn btn-white btn-outline-success waves-effect"><i class="fal fa-edit text-success"></i>Düzenle</button>\n' +
+            '<button type="button" class="btn btn-white btn-outline-success waves-effect"><i class="fal fa-bars text-success"></i>Seçenekler</button>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '<hr>\n' +
+            '<div class="row">\n' +
+            '<div id="answerSection" class="col-8">\n' +
+            '<div class="input-group mb-4">\n' +
+            '<div class="input-group-prepend">\n' +
+            '<span class="input-group-text">Soru Yazınız</span>\n' +
+            '</div>\n' +
+            '<input type="text" class="form-control">\n' +
+            '</div>\n' +
+            '<div class="input-group mb-4">\n' +
+            '<div class="input-group-prepend">\n' +
+            '<span class="input-group-text">Yanıt Giriniz</span>\n' +
+            '</div>\n' +
+            '<input type="text" class="form-control">\n' +
+            '<div class="input-group-append">\n' +
+            '<button id="removeAnswerInput" class="btn btn-md btn-outline-danger m-0 px-3 py-2 z-depth-0 waves-effect" type="button"><i class="fa fa-minus"></i></button>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '<div class="input-group mb-4">\n' +
+            '<div class="input-group-prepend">\n' +
+            '<span class="input-group-text" id="basic-addon1">Yanıt Giriniz</span>\n' +
+            '</div>\n' +
+            '<input type="text" class="form-control">\n' +
+            '<div class="input-group-append">\n' +
+            '<button id="removeAnswerInput" class="btn btn-md btn-outline-danger m-0 px-3 py-2 z-depth-0 waves-effect" type="button"><i class="fa fa-minus"></i></button>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '<div class="input-group mb-4">\n' +
+            '<div class="input-group-prepend">\n' +
+            '<span class="input-group-text" id="basic-addon1">Yanıt Giriniz</span>\n' +
+            '</div>\n' +
+            '<input type="text" class="form-control">\n' +
+            '<div class="input-group-append">\n' +
+            '<button id="removeAnswerInput" class="btn btn-md btn-outline-danger m-0 px-3 py-2 z-depth-0 waves-effect" type="button"><i class="fa fa-minus"></i></button>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '<div class="col-4">\n' +
+            '<select class="form-control" name="" id="">\n' +
+            '<option value="1">Soru Tipi</option>\n' +
+            '<option value="1">Ahmet</option>\n' +
+            '<option value="1">Mehmet</option>\n' +
+            '</select>\n' +
+            '</div>\n' +
+            '<div class="col-12">\n' +
+            '<a id="appendAnswerInput" class=""><i class="fal fa-plus-circle mr-2"></i>Başka bir seçenek ekleniyiz</a>\n' +
+            '</div>\n' +
+            '</div>\n' +
+            '</section>');
+
+});
+
+// headera bastığında soruları kapatan metod
+// $(document).on('click', '.section-header', function(){
+//     $(this).nextAll().slideToggle();
+// });
+
+
+$(document).on('click', '#removeSectionButton', function(){
+    $(this)
+        .parent()
+        .parent()
+        .parent()
+        .parent()
+        .remove();
 });
